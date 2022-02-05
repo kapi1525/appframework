7fa27b70be24ed23f7321419bf3508dae20e5e6d
kapigames <kapigamesdev@gmail.com>
2022-02-05 20:33:28 +0100

    ini: functions are now private and fatal is default loglevel

--- src/appframework/ini.hpp
+++ src/appframework/ini.hpp
@@ -4,11 +4,13 @@
 
 
 
+// TODO: Make ini::save() and ini::parse() not override all comments
+
 // Simple .ini parser
 class ini {
 public:
     ini();
-    ini(std::filesystem::path file_path, logs::loglevel log_level = logs::loglevel::error);
+    ini(std::filesystem::path file_path, logs::loglevel log_level = logs::loglevel::fatal);
     ~ini();
 
 
@@ -18,11 +20,16 @@ public:
 
     std::vector<ini_group_t> data;
 
+    std::string get_item(std::string_view item);                    // Gets item value from global group
+    std::string get_item_group(std::string_view group, std::string_view item);
+
+    void set_item(std::string_view item, std::string_view value);   // Sets item value in global group
+    void set_item_group(std::string_view group, std::string_view item, std::string_view value);
 
     bool parse(std::filesystem::path file_path);                    // Loads ini from file and parses it
-    // TODO: Make ini::save() and ini::parse() not override all comments
     bool save(std::filesystem::path file_path);                     // Saves ini to file (currently saving ini removes all comments)
 
+private:
     bool has_whitespace(std::string_view text);                     // True if has whitespace
     std::string remove_whitespace(std::string_view line);           // Removes whitespaces at start and end
     std::string remove_comments(std::string_view line);             // Removes comments
@@ -33,12 +40,6 @@ public:
 
     bool is_item(std::string_view line);                            // True if passed string is item declaration in ini format
     bool is_group(std::string_view line);                           // True if passed string is group declaration in ini format
-
-    std::string get_item(std::string_view item);                    // Gets item value from global group
-    std::string get_item_group(std::string_view group, std::string_view item);
-
-    void set_item(std::string_view item, std::string_view value);   // Sets item value in global group
-    void set_item_group(std::string_view group, std::string_view item, std::string_view value);
 
     logs log;
 };
