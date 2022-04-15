ba7b78ade38a5b34ad604f3f4caa750e4a5538b6
kapigames <kapigamesdev@gmail.com>
2022-04-15 22:43:34 +0200

    all.hpp

---
+++ src/apf/all.hpp
@@ -0,0 +1,11 @@
+#pragma once
+// This header includes every part of apf
+
+#include "../appframework.hpp"
+
+#include "args.hpp"
+#include "file.hpp"
+#include "term.hpp"
+#include "logs.hpp"
+#include "process.hpp"
+#include "tree.hpp"
