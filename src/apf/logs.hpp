679180c35c4eb84d6ed3b9e089be5b51e6e407a3
kapigames <kapigamesdev@gmail.com>
2022-04-27 00:13:40 +0200

    logs.hpp refactor, apf::log is now a singleton class with support for logging levels

--- src/apf/logs.hpp
+++ src/apf/logs.hpp
@@ -5,88 +5,149 @@
 
 
 namespace apf {
-    namespace log {
-        inline void fatal(std::string_view message);
-        inline void error(std::string_view message);
-        inline void  warn(std::string_view message);
-        inline void  info(std::string_view message);
-        inline void  note(std::string_view message);
 
-        inline void custom(std::string_view log_type, std::string_view message);
+    enum class log_level {
+        none,
+        fatal,
+        error,
+        warn,
+        info,
+        max,
+    };
+
+
+    class log {
+    public:
+        static log& get();
+
+        static void fatal(std::string_view message);
+        static void error(std::string_view message);
+        static void warn(std::string_view message);
+        static void info(std::string_view message);
+        static void note(std::string_view message);
+
+        static void custom(std::string_view log_label, std::string_view message, log_level lvl = log_level::max, rgb label_color = {255, 255, 255}, rgb label_back_color = {0, 0, 0});
+
+        static void set_level(log_level lvl);
+        static log_level get_level();
+
+
+    private:
+        log() = default;
+
+        void set_level_impl(log_level lvl);
+        log_level get_level_impl();
+
+        log_level level = log_level::max;
+    };
+}
+
+
+
+/**
+ * @brief Get log class instance.
+ */
+inline apf::log& apf::log::get() {
+    static apf::log instance;
+    return instance;
+}
+
+
+/**
+ * @brief Print message.
+ * 
+ * @param message 
+ */
+inline void apf::log::fatal(std::string_view message) {
+    custom(message, "fatal", log_level::fatal, {0, 0, 0}, {255, 0, 0});
+}
+
+/**
+ * @brief Print message.
+ * 
+ * @param message 
+ */
+inline void apf::log::error(std::string_view message) {
+    custom(message, "error", log_level::error, {255, 0, 0});
+}
+
+/**
+ * @brief Print message.
+ * 
+ * @param message 
+ */
+inline void apf::log::warn(std::string_view message) {
+    custom(message, "warn", log_level::warn, {255, 255, 0});
+}
+
+/**
+ * @brief Print message.
+ * 
+ * @param message 
+ */
+inline void apf::log::info(std::string_view message) {
+    custom(message, "info", log_level::info);
+}
+
+/**
+ * @brief Print message.
+ * 
+ * @param message 
+ */
+inline void apf::log::note(std::string_view message) {
+    custom(message, "note", log_level::max);
+}
+
+
+/**
+ * @brief Print message with custom label, colors and logging level.
+ * 
+ * @param log_label 
+ * @param message 
+ * @param lvl 
+ * @param label_color 
+ * @param label_back_color 
+ */
+inline void apf::log::custom(std::string_view log_label, std::string_view message, log_level lvl, rgb label_color, rgb label_back_color) {
+    if(get_level() > lvl) {
+        return;
     }
-}
 
+    apf::term::color(label_color);
 
+    if (label_back_color.red != 0 || label_back_color.green != 0 || label_back_color.blue != 0) {
+        apf::term::back_color(label_back_color);
+    }
 
-/**
- * @brief Prints message as fatal error, fatal error is error from which program cant recover.
- * 
- * @param message
- */
-void apf::log::fatal(std::string_view message) {
-    apf::term::color({0,0,0});
-    apf::term::back_color({255,0,0});
-    apf::term::bold();
-    custom("fatal", message);
-}
-
-
-
-/**
- * @brief Prints message as error.
- * 
- * @param message
- */
-void apf::log::error(std::string_view message) {
-    apf::term::color({255,0,0});
-    apf::term::bold();
-    custom("error", message);
-}
-
-
-
-/**
- * @brief Prints message as warning.
- * 
- * @param message
- */
-void apf::log::warn(std::string_view message) {
-    apf::term::color({255,255,0});
-    custom("warn", message);
-}
-
-
-
-/**
- * @brief Prints message.
- * 
- * @param message
- */
-void apf::log::info(std::string_view message) {
-    custom("info", message);
-}
-
-
-
-/**
- * @brief Prints message.
- * 
- * @param message
- */
-void apf::log::note(std::string_view message) {
-    custom("note", message);
-}
-
-
-
-/**
- * @brief Prints message.
- * 
- * @param log_type
- * @param message
- */
-void apf::log::custom(std::string_view log_type, std::string_view message) {
-    std::cout << "[" << log_type << "] " << message;
+    std::printf("[%s] %s.", std::string(log_label).c_str(), std::string(message).c_str());
     apf::term::reset();
-    std::cout << "\n";
+    std::printf("\n");
+}
+
+
+/**
+ * @brief Set logging level.
+ * 
+ * @param lvl 
+ */
+inline void apf::log::set_level(log_level lvl) {
+    get().set_level_impl(lvl);
+}
+
+inline void apf::log::set_level_impl(log_level lvl) {
+    level = lvl;
+}
+
+
+/**
+ * @brief Get loggin level.
+ * 
+ * @return apf::log_level 
+ */
+inline apf::log_level apf::log::get_level() {
+    return get().get_level_impl();
+}
+
+inline apf::log_level apf::log::get_level_impl() {
+    return level;
 }
--- tests/sandbox.cpp
+++ tests/sandbox.cpp
@@ -6,23 +6,15 @@ using namespace std::chrono_literals;
 
 
 
-void create_join_process() {
-    std::filesystem::current_path("test");
-    apf::process p("./start.sh");
-    std::this_thread::sleep_for(10s);
-    std::cout << p.running() << std::endl;
-    p.interrupt();
-    std::cout << p.get() << std::endl;
-    std::cout << p.join() << std::endl;
-}
-
-
-
 class sandbox : public apf::app {
 public:
     void start() {
-        std::thread t(&create_join_process);
-        t.join();
+        apf::log::note("test");
+        apf::log::info("test");
+        apf::log::warn("test");
+        apf::log::error("test");
+        apf::log::fatal("test");
+        std::cout << "end" << std::endl;
     }
 
     int run() {
